0d1b411acb4ebb9b89078bdb628145241f422e7e
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2022-05-12 11:35:33 +0300

    First dummy run with template code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::Error;
+
+fn run(_args: &[String]) -> Result<(), &'static str> {
+
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+
+    std::process::exit(match run(&args[1..]) {
+        Ok(_) => exitcode::OK,
+        Err(err) => {
+            eprintln!("error: {:?}", err);
+            exitcode::USAGE
+        }
+    });
 }
